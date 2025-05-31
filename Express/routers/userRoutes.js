61d6773ffb3dd9ccc603ef7adf85e4d85f4c45d5
vikraam <vikraam17@gmail.com>
2025-05-31 13:22:36 +0530

    signup added

---
+++ Express/routers/userRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const userRouter = express.Router();
+const { SignUp, Login } = require('../controllers/userController')
+userRouter.post('/signup',SignUp);
+userRouter.post('/login',Login);
+
+module.exports = userRouter
